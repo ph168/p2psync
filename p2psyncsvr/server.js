2748bf6e161090fb9db096b9bba3ce27bd133514
Peter Hoffmann <peter@p-h.im>
2015-02-13 02:53:02 +0100

    enable CORS on server

--- p2psyncsvr/server.js
+++ p2psyncsvr/server.js
@@ -17,6 +17,11 @@ server.on('disconnect', function(id) {
 var express = require('express')
 var app = express();
 app.listen(3000);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/peers', function(req, res) {
   return res.json(peers);
 });
